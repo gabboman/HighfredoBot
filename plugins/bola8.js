45c01129a1c1de1c1942c7bb39d909ac15a04a8b
Gabriel Amador García <gabriel.amador.garcia@gmail.com>
2015-11-21 00:56:52 +0100

    MUCHO MOLA

--- plugins/bola8.js
+++ plugins/bola8.js
@@ -35,7 +35,8 @@ module.exports = function(bot) {
         'Si @juanca1991 dejara de mandar porno te podría decir la respuesta',
         'Rezale a tu Dios',
         'Yodeadodoyodeadodoyodeadodoyodeadodo\nyodeadodoyodeadodoyo-bab-baaaaa\nAhhhhhh-aaahhhh-aaaaaa-aaaaAAA!\nOhhhhhh-ooohhh-oooooo-oooOOO!\n\nYodeadodoyodeadodoyodeadodoyodeadodo\nyodeadodoyodeadodoyo-bab-baaaaa\nAhhhhhh-aaahhhh-aaaaaa-aaaaAAA!\nOhhhhhh-ooohhh-oooooo-oooOOO!\n\numdub-adaoh-segel-ungucur-ungetu-hungetur-hupreyu\nundubea-unpedurl-humpelilly-luptodoro-licktetor-ulumpadero\numbader-lickatine-lupator-lackatera\nbatickatheplalera\ntheblumpalumpadera\nho?\nho ho ha haaaa!\n\nYodeadodoyodeadodoyodeadodoyodeadodo\nyodeadodoyodeadodoyo-bab-baaaaa\nAhhhhhh-aaahhhh-aaaaaa-aaaaAAA!\nOhhhhhh-ooohhh-oooooo-oooOOO!\n\nBom bom bom bom\nBom bom bom\nBom bom bom bom bom bom bom\nBom bom bom\nBom bom bom\nBom bom bom bom bom bom \nBac bac bac bac bac backaaaaa!\nYeeeeha!\n\nba um um um um um\nba um um um um um\nba oooohhboooobooboboboooo!\n\nYodeadodoyodeadodoyodeadodoyodeadodo\nyodeadodoyodeadodoyo-bab-baaaaa\nAhhhhhh-aaahhhh-aaaaaa-aaaaAAA!\nOhhhhhh-ooohhh-oooooo-oooOOO!\nYeaah! Whoooo!\n',
-        'PUEDES SER EL MAS MODERNO PERO EL DÍA DE TU ENTIERRO NO TE LLORARÁ NI DIOS *lo siento, el bot está escuchando música, y le mola mucho*'
+        'PUEDES SER EL MAS MODERNO PERO EL DÍA DE TU ENTIERRO NO TE LLORARÁ NI DIOS *lo siento, el bot está escuchando música, y le mola mucho*',
+        'OJALÁ ME ENTRETUVIERA LA ÉPICA GRIEGA\n OJALÁ LA HEREDERA DE LA HISTORIA ENTERA \n FUERA LA ÚNICA PIEZA QUE ESCONDE LA CANCIÓN ETERNA\nQUE RECOPILA LA NOCHE MÁS PLACENTERA DE NUESTRAS VIDAS\nOJALÁ MI MÚSICA FUERA\nSETENTERA\nY MIS DRAMAS TERMINARAN EN LA ACERA VOMITANDO LÁGRIMAS NEGRAAAS\nPODRIAIIIIS IIIIIIIIIIROS TODOS A LA MIEEEERDAAAAAAA \n '
         ];
 
     var exec = function(msg) {
