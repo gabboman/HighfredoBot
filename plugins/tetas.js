9db09a99e46ac7c03a45eaa9b322d2ab368db284
Gabriel Amador García <gabriel.amador.garcia@gmail.com>
2016-06-08 12:59:10 +0200

    mejores tetas

--- plugins/tetas.js
+++ plugins/tetas.js
@@ -7,9 +7,16 @@ module.exports = function(bot) {
 
     var name = "tetas";
     var description = "foto tetas";
-
+    var frases=[
+    'Mira que eres pesado con las tetas',
+    'no sabes abrir una pagina porno tu solo?',
+    'Te has planteado mirar el código y mirar el api tu mismo?',
+    'Eres tan fe@ que pides fototetas a un bot']
     var exec = function(msg, reply) {
-      reply.sendMessage("Plugin desactivado hasta nuevo aviso");
+
+
+      bot.sendMessage(msg.chat.id, frases[_.random(frases.length - 1)]);
+      //reply.sendMessage("Plugin desactivado hasta nuevo aviso");
       /*  request({url:'http://api.oboobs.ru/noise/1', json:true}, function (error, response, body) {
             if (!error && response.statusCode == 200 && body.length > 0) {
                 reply.sendChatAction('upload_photo');
